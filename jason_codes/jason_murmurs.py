a0250f792fc0472e0bc89c70638949e0f0966447
Chih-Sheng <note0972@gmail.com>
2019-04-01 15:19:23 +0800

    Update jason_murmurs.py

--- jason_codes/jason_murmurs.py
+++ jason_codes/jason_murmurs.py
@@ -4,7 +4,16 @@ from jason_codes.for_loop_tester import *
 def my_one_day_work():
     print_ladder(10)
 
+
+def jack_say_something():
+    ###
+    ## TODO: Jack 你要對我說幾句幹話嗎?
+    ###
+    message = ""
+    print(message)
+
 if __name__ == '__main__':
     print("Jason: Hey, My name is Jason :D")
     # jack_wake_up()
     # my_one_day_work()
+    # jack_say_something()
