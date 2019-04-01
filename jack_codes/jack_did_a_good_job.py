abf5d716d28c2206fc4a6ff68256401673c109ea
Chih-Sheng <note0972@gmail.com>
2019-04-01 19:34:26 +0800

    Revert "Jack_decomment"
    This reverts commit 5941160ea4a3b12b37aba4900c3ad29818a08201.

--- jack_codes/jack_did_a_good_job.py
+++ jack_codes/jack_did_a_good_job.py
@@ -1,12 +1,12 @@
 import time
 
 def jack_wake_up():
-    ##
-    # TODO: Jack 請把 for loop 的註解取消
-    ##
-    for x in range(3):
-        time.sleep(1)
-        print("Z...", end=" ")
+    ###
+    ## TODO: Jack 請把 for loop 的註解取消
+    ###
+    # for x in range(3):
+    #     time.sleep(1)
+    #     print("Z...", end=" ")
 
     print("Jack: Okok..., I'm going to get up :(")
 
