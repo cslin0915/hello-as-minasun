17597971f6cd73d97166593e0fcec9af89a917e7
Chih-Sheng <note0972@gmail.com>
2019-04-01 11:54:14 +0800

    Add the demo of revert files

---
+++ jack_codes/jack_did_a_good_job.py
@@ -0,0 +1,8 @@
+import time
+
+def jack_wake_up():
+    # time.sleep(3)
+    print("Jack: Okok..., I'm going to get up :(")
+
+if __name__ == '__main__':
+    print("Jack: Hey, My name is Jack ;)")
---
+++ jason_codes/jason_murmurs.py
@@ -0,0 +1,5 @@
+from jack_codes.jack_did_a_good_job import *
+
+if __name__ == '__main__':
+    print("Jason: Hey, My name is Jason :D")
+    jack_wake_up()
